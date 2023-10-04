47b972a54cb87ded333f3f512e39f00379dfc410
Tornadoguy26 <prithviajay100@gmail.com>
2023-10-03 23:38:15 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,23 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
-	public static void main(String [] args) {
-
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
+	
+	public static void main(String [] args) throws IOException {
 		
-		double [] c1 = {6,0,0,5};
-
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-
-		Polynomial p2 = new Polynomial(c2);
-
+		Polynomial p = new Polynomial();
+		//File file = new File("C:\\Users\\torna\\eclipse-workspace\\Polynomial\\src\\Test.txt");
+		//Polynomial ft = new Polynomial(file);
+		
+		int []d1 = {0,3};
+		double []c1 = {6,5};
+		
+		int []d2 = {1, 4};
+		double []c2 = {-2, -9};
+		
+		int []d3 = {2, 3, 5};
+		double []c3 = {3.9, 6, -1};
+		
+		Polynomial p1 = new Polynomial(c1, d1);
+		Polynomial p2 = new Polynomial(c2, d2);
+		Polynomial p3 = new Polynomial(c3, d3);		
+		
+		System.out.println(p.evaluate(3));
 		Polynomial s = p1.add(p2);
+		Polynomial m = s.multiply(p3);
+		
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-
+		
+		System.out.println(m.evaluate(2));
+		
 		if(s.hasRoot(1)){
 			System.out.println("1 is a root of s");
-		}else{
+		}else {
 			System.out.println("1 is not a root of s");
 		}
+		//System.out.println(ft.evaluate(1));
+		
+		//s.saveToFile("C:\\Users\\torna\\eclipse-workspace\\Polynomial\\src\\Test2.txt");
 	}
+	
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,35 +1,149 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Polynomial{
 
-	public double []Poly;
+	public double []Coff;
+	public int []Deg;
 	
 	public Polynomial(){
-		Poly = new double[]{0};
+		Coff = new double[]{0};
+		Deg = new int[]{0};
 	}
 
-	public Polynomial(double []Cof){
+	public Polynomial(double []Cof, int []Deg){
 		
-		Poly = new double[Cof.length];
+		Coff = new double[Cof.length];
+		this.Deg = new int[Deg.length];
 
-		for(int i=0; i<Cof.length; i++){
-			Poly[i] = Cof[i];
+		for(int i=0; i< Cof.length; i++){
+			Coff[i] = Cof[i];
+		}
+		
+		for(int j=0; j<Deg.length; j++) {
+			this.Deg[j] = Deg[j];
 		}
 	}
-
-	public Polynomial add(Polynomial b){
-		if(Poly.length >= b.Poly.length){
-			Polynomial newPoly = new Polynomial(Poly);
-			for(int i=0; i < b.Poly.length; i++){
-				newPoly.Poly[i] += b.Poly[i];
+	
+	public Polynomial(File file) throws IOException{
+		
+		BufferedReader b = new BufferedReader(new FileReader(file));
+		String j = b.readLine();
+		b.close();
+		
+		int count = 1;
+		
+		for(int i=0; i<j.length()-1; i++) {
+			if(j.substring(i, i+1).equals("+") || j.substring(i, i+1).equals("-")){
+				count++;
 			}
-			return newPoly;
-		} else {
-			Polynomial newPoly = new Polynomial(b.Poly);
-			for(int i=0; i < Poly.length; i++){
-				newPoly.Poly[i] += Poly[i];
-			}
-			return newPoly;
 		}
+		
+		Coff = new double[count];
+		Deg = new int[count];
+		
+		count = 0;
+		int count2 = 0;
+		int val = 0;
+		double sign = 1;
+		double sign2 = 1;
+		boolean flag = true;
+		
+		for(int i=0; i<j.length()-1; i++) {
+			if(j.substring(i, i+1).equals("+") || j.substring(i, i+1).equals("-")){
+				if(flag) {
+					if(j.substring(0,1).equals("-")) {
+						sign2 = -1;
+						val = 1;
+					} else {
+						sign2 = 1;
+					}
+					Deg[count] = 0;
+					Coff[count2] = sign2*Double.parseDouble(j.substring(val, i));
+					val = i+1;
+					count ++;
+					count2 ++;
+					flag = false;
+					if(j.substring(i, i+1).equals("-")) {
+						sign = -1;
+					}
+				} else {
+					Deg[count] = Integer.parseInt(j.substring(val,i));
+					count++;
+					val = i+1;
+					if(j.substring(i, i+1).equals("-")) {
+						sign = -1;
+					}
+				}
+			} else if(j.substring(i, i+1).equals("x")) {
+				flag = false;
+				Coff[count2] = sign*Double.parseDouble(j.substring(val, i));
+				sign = 1;
+				count2++;
+				val = i+1;
+			}
+		}
+		
+		Deg[count] = Integer.parseInt(j.substring(val, j.length()));
+		
+	}
+	
+	public Polynomial add(Polynomial b){
+		
+		double []a = new double[Arrays.stream(Deg).max().getAsInt()+1];
+		double []a2 = new double[Arrays.stream(b.Deg).max().getAsInt()+1];
+		
+		for(int i=0; i<Deg.length; i++) {
+			a[Deg[i]] = Coff[i];
+		}
+		
+		for(int i=0; i<b.Deg.length; i++) {
+			a2[b.Deg[i]] = b.Coff[i];
+		}
+		
+		// a = [
+		
+		double []c = null;
+		
+		if(a.length >= a2.length){
+			for(int i=0; i < a2.length; i++){
+				a[i] += a2[i];
+			}
+			c = a.clone();
+		} else {
+			for(int i=0; i < a.length; i++){
+				a2[i] += a[i];
+			}
+			c = a2.clone();
+		}
+		
+		int count = 0;
+		
+		for(int i=0; i<c.length; i++){
+			if(c[i] == 0 ) {
+				count++;
+			}
+		}
+		
+		int []x = new int[c.length-count];
+		double []y = new double[c.length-count];
+		int j = 0;
+		
+		for(int i=0; i<c.length; i++) {
+			if(c[i] != 0 || i==0) {
+				x[j] = i;
+				y[j] = c[i];
+				j++;
+			}
+		}
+		
+		Polynomial Poly = new Polynomial(y, x);
+		return Poly;
 			
 			
 	}
@@ -37,11 +151,9 @@ public class Polynomial{
 	public double evaluate(double x){
 		
 		double value = 0;
-		double a = 1;
 
-		for(int i=0; i<Poly.length; i++){
-			value += Poly[i] * a;
-			a *= x;
+		for(int i=0; i<Deg.length; i++){
+			value += Coff[i] * Math.pow(x, Deg[i]);
 		}
 
 		return value;
@@ -50,20 +162,77 @@ public class Polynomial{
 
 	public boolean hasRoot(double x){
 
-		double value = 0;
-		double a = 1;
-		boolean root = true;
-		
-		for(int i=0; i<Poly.length; i++){
-			value += Poly[i] * a;
-			a *= x;
-		}
 
-		if(value != 0){
-			root = false;
+		boolean root = false;
+
+		Polynomial a = new Polynomial(Coff, Deg);
+		if(a.evaluate(x) <= 0.0001){
+			root = true;
 		}
 
 		return root;
 	}
+	
+	public Polynomial multiply(Polynomial b) {
+		
+		double []a2 = new double[Arrays.stream(Deg).max().getAsInt()+Arrays.stream(b.Deg).max().getAsInt()+1];
+		int count = 0;
+		boolean flag = true;
+		
+		for(int i = 0; i<Deg.length; i++) {
+			for(int j=0; j<b.Deg.length; j++) {
+				if(a2[Deg[i]+b.Deg[j]] == 0 ) {
+					count += 1;
+					flag = false;
+				}
+				
+				a2[Deg[i]+b.Deg[j]] += Coff[i]*b.Coff[j];
+				
+				if(a2[Deg[i]+b.Deg[j]] == 0 && flag) {
+					count -= 1;
+				}
+				flag = true;
+			}
+		}
+		
+		double []c = new double[count];
+		int []d = new int[count];
+		int j = 0;
+		
+		for(int i=0; i<a2.length; i++) {
+			if(a2[i]!=0) {
+				c[j] = a2[i];
+				d[j] = i;
+				j++;
+			}
+		}
+		
+		
+		Polynomial Poly = new Polynomial(c, d);
+		return Poly;
+		
+	}
 
+	public void saveToFile(String file) throws IOException {
+		
+		BufferedWriter b = new BufferedWriter(new FileWriter(file));
+		
+		for(int i=0; i<Deg.length; i++) {
+
+			b.write(Double.toString(Coff[i]));
+			if(Deg[i]!=0) {
+				b.write("x");
+				b.write(Integer.toString(Deg[i]));
+			}
+			
+			if(i<Deg.length-1) {
+				if(Coff[i+1]>=0) {
+					b.write("+");
+				} 
+			}	
+			
+		}
+		
+		b.close();
+	}
 }
